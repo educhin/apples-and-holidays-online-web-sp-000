1a5ecb1481f94d74bc29437adf7de88dccb94474
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-02 14:41:09 +0000

    Automatically backed up by Learn

--- spec/holiday_spec.rb
+++ spec/holiday_spec.rb
@@ -1,21 +1,21 @@
 describe "holiday_supplies_hash" do
 # Hash of seasons, the holidays in them, and some items each holiday might have.
-  let(:holiday_supplies) {
-    {
-      :winter => {
-        :christmas => ["Lights", "Wreath"],
-        :new_years => ["Party Hats"]
-      },
-      :summer => {
-        :fourth_of_july => ["Fireworks", "BBQ"]
-      },
-      :fall => {
-        :thanksgiving => ["Turkey"]
-      },
-      :spring => {
-        :memorial_day => ["BBQ"]
-      }
-    }
+  # let(:holiday_supplies) {
+  #   {
+  #     :winter => {
+  #       :christmas => ["Lights", "Wreath"],
+  #       :new_years => ["Party Hats"]
+  #     },
+  #     :summer => {
+  #       :fourth_of_july => ["Fireworks", "BBQ"]
+  #     },
+  #     :fall => {
+  #       :thanksgiving => ["Turkey"]
+  #     },
+  #     :spring => {
+  #       :memorial_day => ["BBQ"]
+  #     }
+  #   }
   }
 
  # Question 1

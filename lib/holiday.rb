08169eee4e9d54367c9903e1e09e23139cb35621
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-02 15:14:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,7 +91,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   bbq_list = []
     holiday_hash.each do |season, data|
-      bbq_list << season.key if data.values.flatten.include?("BBQ")
+      data.each do |holiday, supplies|
+      bbq_list << holiday if supplies.include?("BBQ")
     end
   bbq_list
 end
